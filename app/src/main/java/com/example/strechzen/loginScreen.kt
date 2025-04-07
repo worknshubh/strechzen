c1ab4d51e37bb5af73a0a074d943bb4a8b28dd6f
worknshubh <worknshubh@outlook.com>
2025-04-07 20:43:36 +0530

    Minor changes (screen linking)

--- app/src/main/java/com/example/strechzen/loginScreen.kt
+++ app/src/main/java/com/example/strechzen/loginScreen.kt
@@ -1,18 +1,25 @@
 package com.example.strechzen
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.math.log
 
 class loginScreen : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login_screen)
-
-
+        var login_btn = findViewById<ImageView>(R.id.login_btn)
+        login_btn.setOnClickListener{
+            startActivity(Intent(this,MainActivity::class.java))
+        }
 
     }
 }
